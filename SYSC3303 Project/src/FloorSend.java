1e0af33530b9a839bf0aaea530972449dced6efa
Shaviyo Marasinghe <shaviyobm@outlook.com>
2019-01-31 16:36:40 -0500

    k

--- SYSC3303 Project/src/FloorSend.java
+++ SYSC3303 Project/src/FloorSend.java
@@ -3,6 +3,10 @@ public class FloorSend {
 	/*
 	 * ->Direction request(Up and Down)
 	 * ->Arrival Sensor(yes or no)
-	 * ->floor display(int of current location of elevator)
+	 * ->floor display(current location of elevator)
 	 */
+	
+	public FloorSend() {
+		
+	}
 }
