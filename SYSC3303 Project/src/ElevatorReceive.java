09eee655e7913560efaa7c1c1cbb98290351c877
Brian Philip <32573732+Brian2197@users.noreply.github.com>
2019-01-31 15:26:39 -0500

    Update ElevatorReceive.java
    Co-Authored-By: Viktor Dimitrov <viktordimitrov@users.noreply.github.com>

--- SYSC3303 Project/src/ElevatorReceive.java
+++ SYSC3303 Project/src/ElevatorReceive.java
@@ -7,28 +7,44 @@ public class ElevatorReceive {
 	public static boolean isRequest;
 	public static int requestFloorNumber;
 	
+	byte state = 0x01;
+	
 	ElevatorReceive(byte[] receivedSchedule){
-
-		runElevator(receivedSchedule[1], receivedSchedule[2]);
+		runElevator(receivedSchedule[1], receivedSchedule[2], receivedSchedule[3]);
+		openCloseDoor(receivedSchedule[4]);
 		//isRequest=request;
 		//requestFloorNumber=floorRequest;
 	}
 
-	public void runElevator(byte motorSpinTime, byte currentFloor ) {
+	public void runElevator(byte motorDirection, byte motorSpinTime, byte currentFloor ) {
 		int time = (int)motorSpinTime;
 		int button = (int)currentFloor;
 
-		while (time != 0){
-			try {
-				System.out.println(button);
-				Thread.sleep(1000);
-				time--;
-				button++;
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		if(motorDirection == 1 || motorDirection == 2) {
+			while (time != 0){
+				try {
+					System.out.println(button);
+					Thread.sleep(1000);
+					time--;
+					if(motorDirection == 1) {
+						button++;
+					} else {
+						button--;
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} 
+		}else if (motorDirection ==0) {
+			
 		}
 	}
 	
-
+	public void openCloseDoor(byte door) {
+		if (door == 1) {
+			System.out.println("doors are open");
+		} else if (door == 0) {
+			System.out.println("doors are closing");
+		}
+	}
 }
