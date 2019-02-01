4cddba85742cdd6aeacff4cd209bc76388ff814f
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2019-02-01 17:14:31 -0500

    Update ElevatorTest.java

--- SYSC3303 Project/src/ElevatorTest.java
+++ SYSC3303 Project/src/ElevatorTest.java
@@ -1,69 +1,21 @@
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Test;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-class ElevatorTest extends Elevator {
+//import static org.assertj.core.api.Assertions.*;
 
-	public ElevatorTest(String name) {
-		super(name);
-		mockScheduler();
-		TestElevatorSendPacket();
-		TestDoorOpen();
-		TestRunELevator();
+class ElevatorTest {
 
-	}
-
-	//Mock Scheduler
-	public void mockScheduler() {
-		// data being sent from the scheduler to the Elevator
-		byte motorDirection = 0;
-		byte motorSpinTime = 1;
-		byte currentFloor = 3;
-		
-	}
 	
-	 @Before
-	    public void setUp() throws Exception {
-		 Elevator elevator1 = new Elevator("1");
+	 @Test
+    public void exists() throws Exception {
+        assertThat(new Elevator()).isNotNull();
 	 }
 	 
-	@Test
-	
-	public void ElevatorCreated() {
-		Elevator elevator1 = new Elevator("1");
-		
-		assertEquals(elevator1, elevator1);
-	}
-	@Test
-	void TestElevatorSendPacket() {
-		//Elevator elevator1 = new Elevator("1");
-		Assert.assertEquals(1, 1);
-		//
-	}
-	
-	@Test
-	void TestDoorOpen() {
-		Elevator elevator1 = null;
-		
-		elevator1.openCloseDoor((byte)1);
-		
-	}
-	
-	@Test 
-	void TestRunELevator() {
-		
-	}
-	
-	
-	void test() {
-
-		fail("Not yet implemented");
-	}
-
 }
