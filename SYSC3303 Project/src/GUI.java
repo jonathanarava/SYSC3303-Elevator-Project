f8e77a734c9fd2a108bd01a25315652fe60f3044
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2019-03-28 13:51:39 -0400

    Update GUI.java

--- SYSC3303 Project/src/GUI.java
+++ SYSC3303 Project/src/GUI.java
@@ -1,8 +1,13 @@
 import java.awt.BorderLayout;
 import java.awt.ComponentOrientation;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import javafx.scene.text.Font;
 
 public class GUI {
 
@@ -24,6 +29,25 @@ public class GUI {
 			button = new JButton("Elevator " + i);	
 			frame.add(button);
 		}
+		
+	
+		JTextArea textArea = new JTextArea(
+			    "This is an editable JTextArea. " +
+			    "A text area is a \"plain\" text component, " +
+			    "which means that although it can display text " +
+			    "in any font, all of the text is in the same font."
+			);
+			//textArea.setFont(new Font("Serif", Font.ITALIC, 16));
+			textArea.setLineWrap(true);
+			textArea.setWrapStyleWord(true);
+			textArea.setEditable(false);
+			
+		JScrollPane areaScrollPane = new JScrollPane(textArea);
+		areaScrollPane.setVerticalScrollBarPolicy(
+		                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		areaScrollPane.setPreferredSize(new Dimension(250, 100));
+		
+		frame.add(areaScrollPane);
 		
 		//Set Layout to frame
 		frame.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
