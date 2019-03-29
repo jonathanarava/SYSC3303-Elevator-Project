9575366e6cf3015d42616255ee342961a97cbf00
Shaviyo Marasinghe <shaviyobm@outlook.com>
2019-03-29 11:58:36 -0400

    Update FloorIntermediate.java

--- SYSC3303 Project/src/FloorIntermediate.java
+++ SYSC3303 Project/src/FloorIntermediate.java
@@ -129,7 +129,6 @@ public class FloorIntermediate extends Thread {
 			try {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
