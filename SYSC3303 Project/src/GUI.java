d5dd84b1e9b700b3ee1656ca040c3be1bdfd8185
Jonathan Arava <32462175+jonathanarava@users.noreply.github.com>
2019-04-04 18:41:50 -0400

    Update

--- SYSC3303 Project/src/GUI.java
+++ SYSC3303 Project/src/GUI.java
@@ -2,6 +2,9 @@ import java.awt.BorderLayout;
 import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Panel;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -21,29 +24,39 @@ GUI() {
 		//Button(s) & TesxtArea initialization
 		JButton button[] = new JButton[numElevators] ;
 		JTextArea textArea[] = new JTextArea[numElevators];
+		Panel pnl1 = new Panel();
+		Panel pnl2 = new Panel();
 		
 		//Layout Initialization
-		FlowLayout experimentLayout = new FlowLayout();
-		frame.setLayout(experimentLayout);	// Set frame layout to FlowLayout
+		FlowLayout flowLayout = new FlowLayout();
+		GridLayout gridLayout = new GridLayout();
+		pnl1.setLayout(gridLayout);	// Elevator Labels
+		pnl2.setLayout(flowLayout);	// Current floors
+		frame.setLayout(flowLayout);
+		
 		
 		// Dynamically creates the number of buttons/ textAreas depending on numOfElevators 
 		// initialized in elevator intermediate class
+		
+		//Buttons
 		for(int i = 0; i< numElevators; i++) {
 			button[i] = new JButton("Elevator " + i);	
-			frame.add(button[i]);
+			pnl1.add(button[i]);
 		}
+		
+		//CurrentFloor
 		for(int i = 0; i< numElevators; i++) {
-			textArea[i] = new JTextArea("Current FLoor " + elevatorCurrentFloor[i]);	
-			frame.add(textArea[i]);
+			textArea[i] = new JTextArea("Current FLoor " /*+ elevatorCurrentFloor[i]*/);	
+			pnl2.add(textArea[i]);
 		}
-	
+		
+		
 		JTextArea TextErrorArea = new JTextArea(
 			    "Elevator Error Message: \n" +
 			    "A text area is a \"plain\" text component, " +
 			    "which means that although it can display text " +
 			    "in any font, all of the text is in the same font."
 			);
-			//textArea.setFont(new Font("Serif", Font.ITALIC, 16));
 		TextErrorArea.setLineWrap(true);
 		TextErrorArea.setWrapStyleWord(true);
 		TextErrorArea.setEditable(false);
@@ -53,6 +66,8 @@ GUI() {
 		                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		areaScrollPane.setPreferredSize(new Dimension(250, 100));
 		
+		frame.add(pnl1);
+		frame.add(pnl2);
 		frame.add(areaScrollPane);
 		
 		//Set Layout to frame
