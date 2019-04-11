4b3ca0904e17f34cb593c8c2b4b682aa847c7670
Shaviyo Marasinghe <shaviyobm@outlook.com>
2019-04-10 21:54:41 -0400

    Update Floor.java

--- SYSC3303 Project/src/Floor.java
+++ SYSC3303 Project/src/Floor.java
@@ -7,26 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/*
- * class Floor:has direction buttons and Floor display
+/**
+ * class Floor: Each floor thread acts as one of the 22 floors. Has direction buttons and Floor display
+ * takes Arguments for how many floors you would like in Floor Subsystem(max 22)
  */
 public class Floor extends Thread {
 
 	public boolean update; // an update has been set
-	/*
-	 * //UNIFIED CONSTANTS DECLARATION FOR ALL CLASSES private static final byte
-	 * HOLD = 0x00;//elevator is in hold state private static final byte UP =
-	 * 0x02;//elevator is going up private static final byte DOWN = 0x01;//elevator
-	 * is going down private static final int ELEVATOR_ID=21;//for identifying the
-	 * packet's source as elevator private static final int FLOOR_ID=69;//for
-	 * identifying the packet's source as floor private static final int
-	 * SCHEDULER_ID=54;//for identifying the packet's source as scheduler private
-	 * static final int DOOR_OPEN=1;//the door is open when ==1 private static final
-	 * int DOOR_DURATION=4;//duration that doors stay open for private static final
-	 * int REQUEST=1;//for identifying the packet sent to scheduler as a request
-	 * private static final int UPDATE=2;//for identifying the packet sent to
-	 * scheduler as a status update private static final int FLOOR = 69;
-	 */
 
 	// UNIFIED CONSTANTS DECLARATION FOR ALL CLASSES
 	// States
@@ -90,11 +77,11 @@ public class Floor extends Thread {
 
 	private boolean hasRequest = false;
 	private static byte error = NO_ERROR;// current Error, will be sent on the next send
-	/*
-	 * Constructor so Floors can be initialized in a way that can be runnable in the
-	 * scheduler
-	 */
-
+	
+/**
+ * Constructor so Floors can be initialized in a way that can be runnable in the
+ * scheduler
+ */
 	public Floor(int getName, List<byte[]> floorTable) {// int numOfFloors) {
 		// NAMING = name;// mandatory for having it actually declared as a thread object
 		// use a numbering scheme for the naming
@@ -102,7 +89,12 @@ public class Floor extends Thread {
 		name = getName;
 		this.floorTable = floorTable;
 	}
-
+/**
+ * 
+ * @param requestUpdateError: Weather this is an Error, or a request to be sent to the scheduler
+ * @param sendErrorType: if error, what type of error
+ * Sends a packet to the scheduler from the Synchronized table that the floor threads use
+ */
 	public synchronized void sendPacket(int requestUpdateError, byte sendErrorType) {
 		synchronized (floorTable) {
 			while (floorTable.size() != 0) {// there is another packet waiting to be sent, wait for an opening to send
@@ -118,7 +110,12 @@ public class Floor extends Thread {
 			floorTable.notifyAll();
 		}
 	}
-
+/**
+ * 
+ * @param requestUpdateError: Weather this is an Error, or a request to be sent to the scheduler
+ * @param errorType: if error, what type of error
+ * @return returns a byte array of the responsePacket that sendPacket() method will use
+ */
 	public byte[] createResponsePacketData(int requestUpdateError, byte errorType) {// create the Data byte[] for
 		// the response packet to be
 		// sent to the scheduler
@@ -163,19 +160,21 @@ public class Floor extends Thread {
 		requestElevator.write(UNUSED); // no errors
 		return requestElevator.toByteArray();
 	}
-
+/**
+ * 
+ * @param upOrDown: Sets a realTime request on the specified floor Up or Down 
+ */
 	public void setRealTimeRequest(int upOrDown) {
 		hasRequest = true;
 		realTimeDirectionRequest = upOrDown;
 	}
 
-	// SAME AS ELEVATOR'S METHOD
-	/*
-	 * public void updateDisplay() { System.out.println("On Floor: " + sensor); if
-	 * (isGoingUp) { System.out.println("Going Up"); } else if (!isGoingUp) {
-	 * System.out.println("Going Down"); } }
-	 */
-
+/**
+ * 
+ * @param whichElevator: Which elevator is arriving
+ * @param onFloor: On which floor is this message to be updated 
+ * @param schedularInstruction: What instruction was given to the elevator
+ */
 	public void updateDisplay(int whichElevator, int onFloor, int schedularInstruction) {
 		System.out.println("Floor " + name + ": Elevator " + whichElevator + " is Currently on Floor  " + onFloor);
 		if (schedularInstruction == UP || schedularInstruction == DOWN) {
@@ -202,7 +201,9 @@ public class Floor extends Thread {
 			System.out.println("floor given is: " + onFloor);
 		}
 	}
-
+/**
+ * Thread will wait indefinitely for a real time request from the floorIntermediate
+ */
 	public synchronized void waitForRequest() {
 		while (!hasRequest) {
 			try {
@@ -214,18 +215,13 @@ public class Floor extends Thread {
 		}
 	}
 
-	/*
-	 * public void LEDOnOrOff(byte up_or_down, ) { while (schedulerInstruction !=
-	 * true) { if (NAMING =) } }
-	 */
 
-	/*
-	 * Takes in a .txt file as a string. 1st and 2nd line of of txt file are
-	 * discarded(due to the formatting given in project requirements) Takes the
-	 * input information and creates a list of Strings that will have the real time
-	 * inputs as a string. For now This section will be commented. Will be
-	 * implemented for other iterations
-	 */
+
+/**
+ * 
+ * @param fullFile: Takes in .txt file which contains the realTime information that the system can
+ * use to feed in Requests
+ */
 	public void fileReader(String fullFile) {
 		String text = "";
 		int i = 0;
@@ -248,7 +244,9 @@ public class Floor extends Thread {
 			e.printStackTrace();
 		}
 	}
-
+/**
+ * Run method for the thread
+ */
 	public void run() {
 		while (hasRequest) {
 			// while(true) to activate all elevator threads in this system
